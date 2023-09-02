957a711691d4cb0e7c18813d1d7ec72839f39b27
Deaath65 <deaath65@gmail.com>
2023-09-02 08:47:32 +0545

    My Sql connected

--- market/market/settings.py
+++ market/market/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'marketplace',
+        'USER': 'root',
+        'PASSWORD': 'Deaath@65',
+        'HOST': 'localhost',
+        'PORT':'3306',
     }
 }
 
