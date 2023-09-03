c219768dff00fb6517e648fa978c7735361b9a1c
Deaath65 <deaath65@gmail.com>
2023-09-03 19:32:27 +0545

    model-Item

--- market/item/models.py
+++ market/item/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
@@ -6,7 +7,24 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
     
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+    
     def __str__(self):
         return self.name
     
+
     
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='Items', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
