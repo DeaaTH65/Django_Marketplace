7d628ae9ad586495bb8b5c23f94e808a051b24ac
Deaath65 <deaath65@gmail.com>
2023-09-04 08:36:25 +0545

    newest items

--- market/core/views.py
+++ market/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.all()
+    categories = Category.objects.all()
+    
+    context = {'items': items, 'categories': categories}
+    return render(request, 'core/index.html', context)
