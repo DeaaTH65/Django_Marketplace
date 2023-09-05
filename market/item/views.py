84b9c1eca06df1eab4d80a49a412048c915a7a29
Deaath65 <deaath65@gmail.com>
2023-09-05 14:44:21 +0545

    New Item - views

--- market/item/views.py
+++ market/item/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
-from .forms import EditItemForm
+from .forms import EditItemForm, NewItemForm
 
 
 
@@ -8,6 +8,25 @@ from .forms import EditItemForm
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     return render(request, 'item/detail.html', {'item': item})
+
+
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New item',
+    })
 
 
 def edit(request, pk):
