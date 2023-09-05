ec61eaaba1ead68ffd5c180d5dbc04f39d0f4e69
Deaath65 <deaath65@gmail.com>
2023-09-05 10:23:37 +0545

    Signup

--- market/core/views.py
+++ market/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
+from .forms import SignUpForm
+from django.contrib.auth import authenticate, login
 
 
 
@@ -14,3 +16,20 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'core/signup.html', {'form': form})
