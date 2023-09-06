4a19d2d827416402852d0192a34864b44e357c65
Deaath65 <deaath65@gmail.com>
2023-09-06 18:30:53 +0545

    browse

--- market/item/views.py
+++ market/item/views.py
@@ -6,9 +6,16 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+    
+    return render(request, 'item/items.html', {'items': items})
+
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    return render(request, 'item/detail.html', {'item': item})
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)
+    return render(request, 'item/detail.html', {'item': item, 'related_items': related_items})
 
 
 @login_required(login_url='login')
