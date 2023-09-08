fe5ce171bba59841ae058032bbb020b2045bec76
Deaath65 <deaath65@gmail.com>
2023-09-08 11:32:03 +0545

    new conversation

--- market/chat/views.py
+++ market/chat/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from item.models import Item
 from .models import Conversation
 from .forms import ConversationMessageForm
+from django.contrib.auth.decorators import login_required
 
 
 
 
 # Create your views here.
+@login_required
 def new_conversation(request, item_pk):
     item = get_object_or_404(Item, pk=item_pk)
     
@@ -15,6 +17,27 @@ def new_conversation(request, item_pk):
     
     conversations = Conversation.objects.filter(item=item).filter(members__in=[request.user.id])
     
+    if conversations:
+        pass
+    
+    if request.method == 'POST':
+        form = ConversationMessageForm(request.POST)
+        
+        if form.is_valid():
+            conversation = Conversation.objects.create(item=item)
+            conversation.members.add(request.user)
+            conversation.members.add(item.created_by)
+            conversation.save()
+
+            conversation_message = form.save(commit=False)
+            conversation_message.conversation = conversation
+            conversation_message.created_by = request.user
+            conversation_message.save()
+
+            return redirect('item:detail', pk=item_pk)
+    else:
+        form = ConversationMessageForm()
+    
     form = ConversationMessageForm()
-    return render(request, 'conversation/new.html', {'form': form})
+    return render(request, 'chat/new.html', {'form': form})
     
--- market/market/urls.py
+++ market/market/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('chat.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
