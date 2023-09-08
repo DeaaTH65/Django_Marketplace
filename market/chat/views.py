60e9420db76232c89ad0210e7569320d6048d833
Deaath65 <deaath65@gmail.com>
2023-09-08 12:08:45 +0545

    message detail

--- market/chat/views.py
+++ market/chat/views.py
@@ -18,7 +18,7 @@ def new_conversation(request, item_pk):
     conversations = Conversation.objects.filter(item=item).filter(members__in=[request.user.id])
     
     if conversations:
-        pass
+        return redirect('conversation:detail', pk=conversations.first().id)
     
     if request.method == 'POST':
         form = ConversationMessageForm(request.POST)
@@ -49,3 +49,23 @@ def inbox(request):
     return render(request, 'chat/inbox.html', {'conversations': conversations})
     
     
+@login_required
+def detail(request, pk):
+    conversation = Conversation.objects.filter(members__in=[request.user.id]).get(pk=pk)
+
+    if request.method == 'POST':
+        form = ConversationMessageForm(request.POST)
+
+        if form.is_valid():
+            conversation_message = form.save(commit=False)
+            conversation_message.conversation = conversation
+            conversation_message.created_by = request.user
+            conversation_message.save()
+
+            conversation.save()
+
+            return redirect('chat:detail', pk=pk)
+    else:
+        form = ConversationMessageForm()
+
+    return render(request, 'chat/detail.html', {'conversation': conversation, 'form': form})
