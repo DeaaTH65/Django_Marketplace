c4eac3dddbbc44ec79a6011bc5d978e7f475271d
Deaath65 <deaath65@gmail.com>
2023-09-09 09:45:04 +0545

    Profile model

--- market/core/models.py
+++ market/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    profile_img = models.ImageField(null=True, blank=True, upload_to='Profile/')
+    profile_bio = models.CharField(null=True, blank=True, max_length=200)
+    facebook_link = models.CharField(max_length=100, null=True, blank=True)
+    instagram_link = models.CharField(max_length=100, null=True, blank=True)
+    twitter_link = models.CharField(max_length=100, null=True, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+
+# Create Profile with User Signup
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()        
+post_save.connect(create_profile, sender=User)
